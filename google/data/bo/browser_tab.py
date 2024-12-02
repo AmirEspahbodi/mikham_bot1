54dfb40b571bdefb4f8e685a72af8ebffe9e9cc9
amir_espahbodi <dev.amir.espahbodi@gmail.com>
2024-12-02 09:18:42 +0330

    4

--- google/data/bo/browser_tab.py
+++ google/data/bo/browser_tab.py
@@ -67,7 +67,7 @@ class BrowserTabBo(BaseSearchBo):
             await page.wait_for_timeout(randint(7000, 10000))
         except TimeoutError as e:
             page_content = await page.content()
-        
+        print("********** writing goto to goto_google.html")
         with open("../goto_google.html", 'w') as fw:
             fw.write(page_content)
                 
