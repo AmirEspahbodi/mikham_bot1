d40991c47e95c9bd0c394671c4194806b03ca6fb
amir_espahbodi <dev.amir.espahbodi@gmail.com>
2024-12-02 09:39:48 +0330

    7

--- importer/data/bo/browser_tab.py
+++ importer/data/bo/browser_tab.py
@@ -14,19 +14,24 @@ class BrowserTabBo:
         print("goto mikham ...")
         pages = self.resource.pages
         await asyncio.gather(*[self._visit_page(page) for page in pages])
-        page_content = await pages[0].content()
+
+
+    async def _visit_page(self, page: Page):
+        try:
+            await page.goto(self.start_url, timeout=120000)
+
+            # after visiting page we must authenticate to system, for some case we msut import ads on guest mode, so wee need these selectors to be load
+            await page.wait_for_selector(
+                '//div[contains(@class, "header-right")]//div[contains(@class, "header-button")]'
+            )
+            await page.wait_for_selector(
+                '//div[contains(@class, "header-right")]//div[contains(@class, "signin-area")]'
+            )
+        except Exception as e:
+            pass
+
+        page_content = await page.content()
         
         print("********** writing goto to goto_mikham.html")
         with open("../goto_mikham.html", 'w') as fw:
             fw.write(await page_content)
-
-    async def _visit_page(self, page: Page):
-        await page.goto(self.start_url, timeout=120000)
-
-        # after visiting page we must authenticate to system, for some case we msut import ads on guest mode, so wee need these selectors to be load
-        await page.wait_for_selector(
-            '//div[contains(@class, "header-right")]//div[contains(@class, "header-button")]'
-        )
-        await page.wait_for_selector(
-            '//div[contains(@class, "header-right")]//div[contains(@class, "signin-area")]'
-        )
